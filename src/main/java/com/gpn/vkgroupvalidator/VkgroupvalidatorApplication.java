61f28d4c526ad77370052ed1ecddb577971ac632
Alkafol <semenovroman8@gmail.com>
2022-11-01 14:59:48 +0300

    feat: initialize project

---
+++ src/main/java/com/gpn/vkgroupvalidator/VkgroupvalidatorApplication.java
@@ -0,0 +1,13 @@
+package com.gpn.vkgroupvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkgroupvalidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkgroupvalidatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gpn/vkgroupvalidator/VkgroupvalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpn.vkgroupvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkgroupvalidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
